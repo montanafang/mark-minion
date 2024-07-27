f34e08aa3ece89c4da5b48b612ef5826095f568e
vakharwalad23 <vakharwala.d@gmail.com>
2024-07-27 20:35:29 +0530

    feat: Add puppeteer dependency and update worker configuration

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import puppeteer from '@cloudflare/puppeteer';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async fetch(req: Request, env: Env) {},
+};
+
+export class Browser {
+	private browser: puppeteer.Browser | undefined = undefined;
+	private env: Env;
+	private req: Request;
+	helper: Helpers;
+
+	constructor(req: Request, env: Env) {
+		this.req = req;
+		this.env = env;
+		this.helper = new Helpers();
+	}
+	async fetch(req: Request): Promise<Response> {
+		const url = new URL(req.url).searchParams.get('url');
+		const enableDetailedResponse = new URL(req.url).searchParams.get('detailed') === 'true';
+		const subpageCrawl = new URL(req.url).searchParams.get('subpage') === 'true';
+		const unnecessaryFilter = new URL(req.url).searchParams.get('unnecessaryfilter') === 'true';
+		const contentType = new URL(req.url).searchParams.get('contenttype') === 'application/json' ? 'json' : 'text';
+
+		if (!url) {
+			return new Response('Please provide a URL', { status: 400 });
+		}
+		if (!this.helper.isValidUrl(url)) {
+			return new Response('Invalid URL', { status: 400 });
+		}
+
+		return new Response('Hello World', { status: 200 });
+	}
+	async ifBrowser() {
+		let tries = 5;
+		while (tries) {
+			if (!this.browser || !this.browser.isConnected()) {
+				try {
+					// this.browser = await puppeteer.launch();
+				} catch (error) {}
+			}
+		}
+	}
+}
+
+export class Helpers {
+	isValidUrl(url: string): boolean {
+		return url.match(/(http|https):\/\/[^ "]+/) ? true : false;
+	}
+}
